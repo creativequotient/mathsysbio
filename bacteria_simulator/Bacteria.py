6c440d8fdd578f387618c3fe23477629032836a4
Marcus Lee <marcus_lee@u.nus.edu>
2019-10-16 00:49:54 +0800

    Merged all the classes together

--- bacteria_simulator/Bacteria.py
+++ bacteria_simulator/Bacteria.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import copy
 
+
 class Bacteria(object):
     def __init__(this, ID, generation, min_atp, initial_atp):
         """
@@ -11,16 +12,15 @@ class Bacteria(object):
         Use add_product_node(), add_node() and add_edge() to add other nodes and edges.
         """
         this.graph = nx.DiGraph()
-        this.graph.add_node('atp', amount = initial_atp)
+        this.graph.add_node('atp', amount=initial_atp)
         this.min_atp = min_atp
-        this.initial_atp=initial_atp
-        
+        this.initial_atp = initial_atp
+
         this.ID = ID
         this.timestep = 0
         this.generation = generation
 
-    ## Functions to add nodes/edges to the graph 
-    
+    ## Functions to add nodes/edges to the graph
     def add_product_node(this, name, initial_amount, atp_needed):
         """
         Add a node a gene product and hence requires ATP to be synthesized
@@ -31,61 +31,61 @@ class Bacteria(object):
                 (would it be better to associate the atp_needed with the pathway (ie. edge) 
                 instead??? as different pathways may have different ATP requirements)
         """
-        this.graph.add_node(name, amount = initial_amount, atp_fn = lambda produced : produced * atp_needed)
-    
+        this.graph.add_node(name, amount=initial_amount, atp_fn=lambda produced: produced * atp_needed)
+
     ## Add non gene product nodes that don't need ATP (ie. food source nodes)
     def add_node(this, name, initial_amount):
-        this.graph.add_node(name, amount = initial_amount)
-        #print(this.graph.nodes[name]['amount'])
-         
+        this.graph.add_node(name, amount=initial_amount)
+        # print(this.graph.nodes[name]['amount'])
+
     def add_edge(this, tf, gene, weight, evolve_fn):
         if tf not in this.graph or gene not in this.graph:
             raise ValueError('tf or gene node has not been created')
-        this.graph.add_edge(tf, gene, weight=weight, evolve_fn = evolve_fn)
-        #print(this.graph.edges.data())
-        
+        this.graph.add_edge(tf, gene, weight=weight, evolve_fn=evolve_fn)
+        # print(this.graph.edges.data())
+
     ## Determine if cell survives this generation given the quantities of food available
     ## food is a dict of food_source : amount
     def survive(this, food):
         for (food_src, amount) in food.items():
             this.graph.nodes[food_src] = amount
             # this.graph.add_node(food_src,amount=amount)
-        this._next_timestep() # update the graph
-        
+        this._next_timestep()  # update the graph
+
         for food_src in food:
             this.graph.nodes[food_src]['amount'] = 0
-            
+
         return this.graph.nodes['atp']['amount'] >= this.min_atp
 
     ## Increment timestep by 1, update graph
     def _next_timestep(this):
         this.timestep += 1
-        
+
         # update the graph
         increase_by = {}
         for (tf, gene, weight) in this.graph.edges.data('weight'):
             if gene not in increase_by:
                 increase_by[gene] = 0
             gene_produced = weight * this.graph.nodes[tf]['amount']
-            #print("gene produced =" + str(gene_produced))
+            # print("gene produced =" + str(gene_produced))
             increase_by[gene] += gene_produced
-            
+
             # decrease ATP if gene product was produced
             if gene_produced > 0 and this._is_product(gene):
                 this.graph.nodes['atp']['amount'] -= this.graph.nodes[gene]['atp_fn'](gene_produced)
-        
+
         # increase the amounts for each node
         for (node, amount) in increase_by.items():
             if amount > 0:
                 # it's not possible for a TF to actually decrease the amount of protein
                 # only decrease its rate of production (?)
                 this.graph.nodes[node]['amount'] += amount
-        #print(increase_by)
-    
+        # print(increase_by)
+
     # it's a gene product (ptn/rna) if it has an atp function
     def _is_product(this, gene):
         return 'atp_fn' in this.graph.nodes[gene]
-    
+
     ## Clone cell (inherits age and amounts of proteins)
     def clone(this):
         cloned_cell = copy.copy(this)
@@ -95,7 +95,7 @@ class Bacteria(object):
     ## Evolve all edge in this cell
     def evolve(this):
         for (src, dest, evolve_fn) in this.graph.edges.data('evolve_fn'):
-            this.graph.edges[src, dest]['weight'] = evolve_fn(this.graph.edges[src,dest]['weight'])
+            this.graph.edges[src, dest]['weight'] = evolve_fn(this.graph.edges[src, dest]['weight'])
 
     def __str__(this):
         string = f'Bacteria {this.ID}. Generation: {this.generation}. Age: {this.timestep}. ATP threshold: {this.min_atp}'
--- bacteria_simulator/Evolution.py
+++ bacteria_simulator/Evolution.py
@@ -1,9 +1,52 @@
+import numpy as np
+
 class Evolution(object):
 
-    ## Shouldn't need an init
-    def __init__(self):
-        #
+    def __init__(self, initial, sd):
+        """
+        Initialize the Evolution class, an object to handle mutation of weights
+        :param initial: initial weight of edge at generation/time 0
+        :param sd: standard deviation of errors
+        """
+        self.initial = initial
+        self.sd = sd
+        self.weight = initial
 
-    ## Take in current level of enzymatic efficiency and return next/slightly perturbed efficiency
-    def mutate(self, current_efficiency):
-        return 0
+    def evolve(self, time):
+        """
+        Calculate weight given time
+        :param time: time
+        :return: weight
+        """
+        A = 1 / self.initial - 1
+        return 1 / (A * np.exp(-time) + 1)
+
+    def inverse(self, weight):
+        """
+        Returns corresponding time given weight
+        :param weight: weight
+        :return: corresponding time to weight
+        """
+        return np.log(abs((1 / self.initial - 1) / (1 / weight - 1)))
+
+    def error(self):
+        """
+        Generate error given sd
+        :return: error within self.sd
+        """
+        return np.random.normal(scale=self.sd, size=1)
+
+    def getMutated(self):
+        """
+        Generate next mutated weight
+        :return: new weight with slight mutation from the previous weight
+        """
+        self.weight = self.evolve(self.inverse(self.weight) + self.error())
+        if (self.weight > 1):
+            self.weight = 1
+        return self.weight
+
+if __name__ == "__main__":
+    evo = Evolution(0.5, 0.001) # Initialize Evolution object with initial weight and SD
+    for i in range(50):
+        print(evo.getMutated()) # Call .getMutated() to get next weight
