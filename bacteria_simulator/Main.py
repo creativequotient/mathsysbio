c34d73c3dd3a493c14770c9277e32cee44ecb7cb
Chloe Lee <echloelee@gmail.com>
2019-10-19 00:12:11 +0800

    Use IntSimulator

--- bacteria_simulator/Main.py
+++ bacteria_simulator/Main.py
@@ -2,14 +2,14 @@
 This is the Main driver script that will handle the simulation of the system
 """
 from BactSim.Bacteria import make_basic_bacteria
-from BactSim.Simulator import Simulator
+from BactSim.Simulator import Simulator, IntSimulator
 from BactSim.FoodGenerators import FoodGenerator
 from BactSim.FoodGenerators import StaticGenerator
 
 initial_bacteria = make_basic_bacteria(1)
 food_source = StaticGenerator()
-simulator = Simulator(food_generator=food_source,
-                      initial_bacteria=initial_bacteria)
-for i in range(1000):
+simulator = IntSimulator(food_generator=food_source,
+                         initial_bacteria=[initial_bacteria])
+for i in range(20):
     simulator.progress()
     print(len(simulator.bacteria))
